fad22f03ffcc9000f00a9a6f8d28291931eee686
Andrew Krug <andrewkrug@gmail.com>
2025-10-06 21:00:23 -0600

    incorporate JS and CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
 const yaml = require('js-yaml');
-const fs = require('fs');
 
 module.exports = function(eleventyConfig) {
   // Add YAML data support
   eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents));
-
-  // Copy CSS to output
-  eleventyConfig.addPassthroughCopy('src/css');
 
   // Set input and output directories
   return {
